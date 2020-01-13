b3ac3e03a39dbbb556a9eed3591449febe6bac21
Emir Kujundzic <57287197+Emmixx@users.noreply.github.com>
2020-01-13 21:30:31 +0100

    php

---
+++ vjezba54.php
@@ -0,0 +1,10 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "";
+    $port "3309";
+    $coonect = mysqli_connect($server, $user, $password, $database, $port);
+
+
+?>
